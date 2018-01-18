0317ecf5468ad9d43cf8efeba98e9acb8324d3b8
Andrey Kishtov <andreykishtov@gmail.com>
2018-01-18 18:08:09 +0200

    reverse string 3 solutions

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,38 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  return str.split('').reduce((reverse, character) => character + reverse, '');
+}
 
 module.exports = reverse;
+
+// function reverse(str) {
+//   let reversed = '';
+//   for (const iterator of str) {
+//     reversed = iterator + reversed;
+//   }
+//   return reversed;
+// }
+
+// function reverse(str) {
+//   let newString = '';
+//   for (let index = 0; index < str.length; index++) {
+//     newString += str[str.length - index - 1];
+//   }
+//   return newString;
+// }
+
+// function reverse(str) {
+//   return str
+//     .split('')
+//     .map((item, index, arr) => arr[arr.length - index - 1])
+//     .join('');
+// }
+
+// function reverse(str) {
+//   return str
+//     .split('')
+//     .reverse()
+//     .join('');
+// }
